05e99c438c6bb561e556cbf7b4e0ebe402911965
DavidNineRoc <1033404553@qq.com>
2018-01-06 22:56:10 +0800

    :white_check_mark: test builder qrcode

---
+++ tests/QrcodePlusTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests;
+
+
+use DavidNineRoc\Qrcode\QrCodePlus;
+use Endroid\QrCode\QrCode;
+use PHPUnit\Framework\TestCase;
+
+class QrcodePlusTest extends TestCase
+{
+    public function testInstanceOf()
+    {
+        $this->assertInstanceOf(QrCode::class, new QrCodePlus);
+    }
+
+    public function testBuilder()
+    {
+        $qrcode = (new QrCodePlus)->setText('DavidNineRoc')->writeString();
+
+        $this->assertNotFalse((bool)imagecreatefromstring($qrcode));
+    }
+}
