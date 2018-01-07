08728b8ded5fedfab495a855b654c8948662afcf
DavidNineRoc <1033404553@qq.com>
2018-01-07 14:09:03 +0800

    :white_check_mark: test factory

---
+++ tests/FactoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests;
+
+use DavidNineRoc\Qrcode\Factory;
+use DavidNineRoc\Qrcode\QrCodePlus;
+use PHPUnit\Framework\TestCase;
+
+class FactoryTest extends TestCase
+{
+    public function testImageCreate()
+    {
+        $qrcode = (new QrCodePlus('DavidNineRoc'))->writeString();
+
+        $this->assertNotEmpty($qrcode);
+
+        return $qrcode;
+    }
+
+    /**
+     * @depends testImageCreate
+     */
+    public function testFactory($str)
+    {
+        $qrcode = Factory::getInstance($str, ['#087','#047','#017','#080']);
+
+        $this->assertInstanceOf(Factory\Base::class, $qrcode);
+    }
+}
