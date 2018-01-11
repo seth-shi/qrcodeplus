93f4744a67dec59140dcdfe6809099957dd1706a
DavidNineRoc <1033404553@qq.com>
2018-01-11 22:01:25 +0800

    :white_check_mark: 对拥有的提供者进行测试

---
+++ tests/ProvidersTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests;
+
+use DavidNineRoc\Qrcode\Factory;
+use DavidNineRoc\Qrcode\Foundation\Plus;
+use PHPUnit\Framework\TestCase;
+
+class ProvidersTest extends TestCase
+{
+    protected $file = __DIR__ . '/../src/Providers.php';
+
+    public function testProvidersExists()
+    {
+        $this->assertFileExists($this->file);
+    }
+
+    public function testReadabld()
+    {
+        $this->assertFileIsReadable($this->file);
+    }
+
+    public function testProvidersCount()
+    {
+        $providers = require $this->file;
+
+        $this->assertCount(2, $providers);
+
+        return $providers;
+    }
+
+    /**
+     * @depends testProvidersCount
+     */
+    public function testProvidersContent($providers)
+    {
+        $this->assertArrayHasKey('color', $providers);
+        $this->assertArrayHasKey('image', $providers);
+    }
+}
